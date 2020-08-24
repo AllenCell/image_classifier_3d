b46fd6688f1cddde7e41c5d5b37d3f0eabc7b86f
Jianxu Chen <jianxuc@alleninstitute.org>
2020-08-23 23:09:30 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -87,6 +87,6 @@ setup(
     url="https://github.com/aics-int/image_classifier_3d",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
